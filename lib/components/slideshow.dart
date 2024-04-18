c54b395e6334a1158ede0715129d4dfa3f8a68cc
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-18 13:58:14 -0600

    Slideshow - configuraciones usando Provider

--- lib/components/slideshow.dart
+++ lib/components/slideshow.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:slideshow_flutter_app/providers/page_provider.dart';
 
 class Slideshow extends StatelessWidget {
   const Slideshow(
@@ -19,23 +18,27 @@ class Slideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Al declarar el contenedor de los botones en la parte superior, este se ve afectado por el notch, por ello el todo el contenido se declara dentro de un área segura con SafeArea
-    return SafeArea(
-      child: Center(
-        child: Column(
+    // El componente Slideshow usa Provider como gestor de estado para el tema del manejo de las slides
+    return ChangeNotifierProvider(
+      // Durante la creación de la instancia de _PageProvider, le pasamos información de inicialización referente a los colores.
+      create: (_) => _PageProvider(
+          primaryColor: buttonPrimaryColor,
+          secondaryColor: buttonSecondaryColor),
+      // Al declarar el contenedor de los botones en la parte superior, este se ve afectado por el notch, por ello el todo el contenido se declara dentro de un área segura con SafeArea
+      child: SafeArea(
+        child: Center(
+            child: Column(
           children: [
-            if (buttonTop)
-              _Dots(slides.length, buttonPrimaryColor, buttonSecondaryColor),
+            if (buttonTop) _Dots(slides.length),
             // PageView por defecto ocupa el 100% del viewport del dispositivo.
             // Si se desea usar junto a otros widgets, es importanet asignarle un área limitada donde se pueda expandir.
             Expanded(
               child: _Slides(slides),
             ),
 
-            if (!buttonTop)
-              _Dots(slides.length, buttonPrimaryColor, buttonSecondaryColor),
+            if (!buttonTop) _Dots(slides.length),
           ],
-        ),
+        )),
       ),
     );
   }
@@ -63,7 +66,7 @@ class _SlidesState extends State<_Slides> {
     // añadir un listener al controlador de página para que esté atento a cualquier cambio
     pageViewController.addListener(() {
       // Obtener la instancia de mi PageProvider y establecer la nueva página seleccionada
-      Provider.of<PageProvider>(context, listen: false).currentPage =
+      Provider.of<_PageProvider>(context, listen: false).currentPage =
           pageViewController.page!;
     });
     super.initState();
@@ -110,15 +113,11 @@ class _Slide extends StatelessWidget {
 // Widget que agrupa un conjunto de puntos
 class _Dots extends StatelessWidget {
   const _Dots(
-    this.numDots,
-    this.buttonPrimaryColor,
-    this.buttonSecondaryColor, {
+    this.numDots, {
     super.key,
   });
   // El número de puntos a dibujar en pantalla (se debe corresponder con la cantidad de slides)
   final int numDots;
-  final Color buttonPrimaryColor;
-  final Color buttonSecondaryColor;
 
   @override
   Widget build(BuildContext context) {
@@ -127,24 +126,22 @@ class _Dots extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           // El constructor List.generate en Dart permite generar una lista con un tamaño específico y llenarla con elementos generados dinámicamente
-          children: List.generate(numDots,
-              (index) => _Dot(index, buttonPrimaryColor, buttonSecondaryColor)),
+          children: List.generate(numDots, (index) => _Dot(index)),
         ));
   }
 }
 
 // Widget privado para representar un punto o circulo en pantalla
 class _Dot extends StatelessWidget {
-  const _Dot(this.index, this.buttonPrimaryColor, this.buttonSecondaryColor);
+  const _Dot(this.index);
 
   final int index;
-  final Color buttonPrimaryColor;
-  final Color buttonSecondaryColor;
 
   @override
   Widget build(BuildContext context) {
-    // Obtener la instancia de mi PageProvider y leer la página actualmente seleccionada
-    final pageViewIndex = Provider.of<PageProvider>(context).currentPage;
+    // Obtener la instancia de mi PageProvider
+    final pageProvider = Provider.of<_PageProvider>(context);
+
     // AnimatedContainer anima automáticamente los cambios en las propiedades de su contenedor, como alignment, padding, color, decoration, width, height, etc.
     // Es especialmente útil cuando deseas que un contenedor cambie su aspecto de forma animada en respuesta a algún evento, como un botón presionado o un cambio en el estado de la aplicación.
     return AnimatedContainer(
@@ -158,10 +155,50 @@ class _Dot extends StatelessWidget {
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         // Cambiar el color si el indice de este punto se corresponde la página actualmente seleccionada
-        color: (pageViewIndex >= index - .5 && pageViewIndex < index + .5)
-            ? buttonPrimaryColor
-            : buttonSecondaryColor,
+        color: (pageProvider.currentPage >= index - .5 &&
+                pageProvider.currentPage < index + .5)
+            ? pageProvider.buttonPrimaryColor
+            : pageProvider.buttonSecondaryColor,
       ),
     );
+  }
+}
+
+// Provider encargado de admistrar todo lo referente a las páginas o vistas mostradas en el PageView principal de este componente
+// Extiende de ChangeNotifier para notificar los cambios
+class _PageProvider extends ChangeNotifier {
+  _PageProvider({required primaryColor, required secondaryColor}) {
+    buttonPrimaryColor = primaryColor;
+    buttonSecondaryColor = secondaryColor;
+  }
+
+  // Propiedad privada que almacena la página actual
+  double _currentPage = 0;
+  // Estas propiedades se inicializarán de forma parezosa en el contructor de la clase
+  late Color _buttonPrimaryColor;
+  late Color _buttonSecondaryColor;
+
+  // Getter para obtener la página actual
+  double get currentPage => _currentPage;
+
+  // Setter para establecer la página actual
+  set currentPage(double page) {
+    _currentPage = page;
+    // Notificar a todos los listeners que la página actual ha cambiado (los widgets suscritos a este provider deben volver a renderizarse)
+    notifyListeners();
+  }
+
+  // Getter y Setter color primario referente a los botones indicadores
+  Color get buttonPrimaryColor => _buttonPrimaryColor;
+  set buttonPrimaryColor(Color color) {
+    _buttonPrimaryColor = color;
+    notifyListeners();
+  }
+
+  // Getter y Setter color secundario referente a los botones indicadores
+  Color get buttonSecondaryColor => _buttonSecondaryColor;
+  set buttonSecondaryColor(Color color) {
+    _buttonSecondaryColor = color;
+    notifyListeners();
   }
 }
